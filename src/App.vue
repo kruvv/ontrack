7eee105707d20bf89f88d5f705b9ab815bc268f8
kruvv <kruvv@mail.ru>
2024-12-26 11:18:25 +0300

    Добавил панель навигации

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { CheckCircleIcon } from '@heroicons/vue/24/outline'
+import { CheckCircleIcon, ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = ['timeline', 'activities', 'progress']
 </script>
 
 <template>
@@ -16,7 +18,18 @@ import { CheckCircleIcon } from '@heroicons/vue/24/outline'
       </div>
     </a>
   </header>
-  <main></main>
+  <main class="flex flex-grow flex-col"></main>
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-arround border-t">
+      <li v-for="page in navItems" :key="page" class="flex-1">
+        <a :href="`#${page}`" class="flex flex-col items-center p-2 capitalize">
+          <ClockIcon v-if="page === 'timeline'" class="h-6 w-6" />
+          <ListBulletIcon v-else-if="page === 'activities'" class="h-6 w-6" />
+          <ChartBarIcon v-else class="h-6 w-6" />{{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped></style>
